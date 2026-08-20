9b7b25e654aa00ba9d9cf8b8929c1ec8a77c7cd7
enzo herrera <enzoarielherreraortega97@gmail.com>
2026-08-20 01:39:45 -0300

    Si importo cookiParser en app

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import eventsRouter from "./src/routes/events.router.js";
 import sessionsRouter from "./src/routes/sessions.router.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/api/health", (req, res) => {
