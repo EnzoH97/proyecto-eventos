5a19fa5aa01b999575f0625d5c5d99a0ffe91f1d
enzo herrera <enzoarielherreraortega97@gmail.com>
2026-09-08 21:42:47 -0300

    creacion del dao para la ruta events

---
+++ src/dao/event.dao.js
@@ -0,0 +1,21 @@
+import eventRepository from "../repository/event.repository.js";
+
+class EventDAO {
+    async createEvent(eventData) {
+        return await eventRepository.create(eventData);
+    }
+
+    async getEventById(id) {
+        return await eventRepository.findById(id);
+    }
+
+    async getEvents() {
+        return await eventRepository.findAll();
+    }
+
+    async updateEvent(id, eventData) {
+        return await eventRepository.updateById(id, eventData);
+    }
+}
+
+export default new EventDAO();
