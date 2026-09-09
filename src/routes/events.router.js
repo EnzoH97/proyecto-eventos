c140709c8b48e3de7c74ff70a947fb928474d260
enzo herrera <enzoarielherreraortega97@gmail.com>
2026-09-08 21:51:40 -0300

    creacion de las rutas de events

--- src/routes/events.router.js
+++ src/routes/events.router.js
@@ -1,8 +1,41 @@
 import { Router } from "express";
-import { getEvents } from "../controllers/events.controller.js";
+import { authorizeRoles } from "../middlewares/authorizeRole.js";
+import { authorizeEventOwnerOrAdmin } from "../middlewares/authorizeEventOwnerOrAdmin.js"
+import { authenticationMiddleware } from "../middlewares/autheticationMiddleware.js";
+import {createEventController, getEventsController, getEventByIdController, updateEventController} from "../controllers/events.controller.js";
 
 const router = Router();
 
-router.get("/", getEvents);
+
+// -----------------------------------------------------
+// Crear un evento
+// -----------------------------------------------------
+router.post("/", authenticationMiddleware, authorizeRoles(
+    [
+        "organizer", 
+        "admin"
+    ]), createEventController);
+
+// -----------------------------------------------------
+// Listar eventos
+// -----------------------------------------------------
+
+router.get("/", getEventsController);
+
+// -----------------------------------------------------
+// Ver detalle de un evento
+// -----------------------------------------------------
+
+router.get("/:eventId", authenticationMiddleware, getEventByIdController);
+
+// -----------------------------------------------------
+// Editar un evento
+// -----------------------------------------------------
+
+router.put("/:eventId", authenticationMiddleware, authorizeRoles(
+    [
+        "organizer", 
+        "admin"
+    ]), authorizeEventOwnerOrAdmin, updateEventController);
 
 export default router;
