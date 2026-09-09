4f135e08324d23ac78d2e17b5a7cfed7a4837f6a
enzo herrera <enzoarielherreraortega97@gmail.com>
2026-09-08 21:37:19 -0300

    se creo el model basico para eventos

---
+++ src/models/event.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const eventSchema = new mongoose.Schema(
+    {
+        name: {
+        type: String,
+        required: true,
+        trim: true
+        },
+
+        date: {
+        type: Date,
+        required: true
+        },
+
+        capacity: {
+        type: Number,
+        default: 0,
+        min: 0
+        },
+
+        organizer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+const Event = mongoose.model("Event", eventSchema);
+
+export default Event;
