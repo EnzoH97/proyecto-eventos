6d3dedc928f56f2a47aac9eab93566499d5d5ffa
enzo herrera <enzoarielherreraortega97@gmail.com>
2026-09-08 21:40:24 -0300

    creacion del repository para event

---
+++ src/repository/event.repository.js
@@ -0,0 +1,26 @@
+import Event from "../models/event.model.js";
+
+class EventRepository {
+
+    async create(eventData) {
+        return await Event.create(eventData);
+    }
+
+    async findById(id) {
+        return await Event.findById(id);
+    }
+
+    async findAll() {
+        return await Event.find();
+    }
+
+    async updateById(id, eventData) {
+        return await Event.findByIdAndUpdate(id, eventData, {
+        new: true,
+        runValidators: true
+        });
+    }
+
+}
+
+export default new EventRepository();
