e64ed22b53adfb2dd1bea0cd962e7a07c094bcd2
enzo herrera <enzoarielherreraortega97@gmail.com>
2026-09-08 21:53:30 -0300

    creacion de middlewares para autenticacion y autorizacion

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ import eventsRouter from "./src/routes/events.router.js";
 import sessionsRouter from "./src/routes/sessions.router.js";
 import cookieParser from "cookie-parser";
 import passport from "./src/config/passport.config.js";
+import { errorHandler } from "./src/middlewares/errorHandler.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(passport.initialize());
+app.use(errorHandler);
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/api/health", (req, res) => {
@@ -18,7 +20,10 @@ app.get("/api/health", (req, res) => {
     });
 });
 
+
 app.use("/api/events", eventsRouter);
 app.use("/api/sessions", sessionsRouter);
+
+
 
 export default app;
