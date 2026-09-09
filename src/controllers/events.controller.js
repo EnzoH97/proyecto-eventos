3c3cd2bb8d03ed68ea674c2b7f508ab2248524a8
enzo herrera <enzoarielherreraortega97@gmail.com>
2026-09-08 21:49:21 -0300

    creacion del controller para la ruta events

--- src/controllers/events.controller.js
+++ src/controllers/events.controller.js
@@ -1,6 +1,75 @@
-export const getEvents = (req, res) => {
-    res.json({
-        status: "success",
-        payload: []
-    });
+import eventService from "../services/event.service.js";
+
+
+// -----------------------------------------------------
+// CREAR EVENTO
+// -----------------------------------------------------
+
+
+export const createEventController = async (req, res, next) => {
+    try {
+        const { name, date, capacity, organizer } = req.body;
+        const owner = req.user ? req.user._id : organizer;
+        const newEvent = await eventService.createEvent({
+            name,
+            date,
+            capacity
+        }, owner);
+        return res.status(201).json({
+            status: "success",
+            payload: newEvent
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+// -----------------------------------------------------
+// READ ALL
+// -----------------------------------------------------
+
+export const getEventsController = async (req, res, next) => {
+    try {
+        const events = await eventService.getEvents();
+        return res.status(200).json({
+            status: "success",
+            payload: events
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+// -----------------------------------------------------
+// READ ONE
+// -----------------------------------------------------
+
+export const getEventByIdController = async (req, res, next) => {
+    try {
+        const { eventId } = req.params;
+        const event = await eventService.getEventById(eventId);
+        return res.status(200).json({
+            status: "success",
+            payload: event
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+// -----------------------------------------------------
+// UPDATE
+// -----------------------------------------------------
+
+export const updateEventController = async (req, res, next) => {
+    try {
+        const { eventId } = req.params;
+        const event = await eventService.updateEvent(eventId, req.body);
+        return res.status(200).json({
+            status: "success",
+            payload: event
+        });
+    } catch (error) {
+        next(error);
+    }
 };
