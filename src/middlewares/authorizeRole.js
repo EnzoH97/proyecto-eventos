4fbf07ab6c2fd09bfb55ce53028a3bd767754dee
enzo herrera <enzoarielherreraortega97@gmail.com>
2026-09-11 21:11:42 -0300

    actualizacion de los middlewares

--- src/middlewares/authorizeRole.js
+++ src/middlewares/authorizeRole.js
@@ -1,13 +1,21 @@
 
-export const authorizeRoles = (roles) =>{
-    return (req,res,next)=>{
 
-        if(!roles.includes(req.user.role)){
-            return res.status(403).json({
-                status: "error",
-                message: "No tenés permisos para realizar esta acción"
-            })
+export const authorizeRoles = (...allowedRoles) => {
+    return (req, res, next) => {
+        if (!req.user) {
+        return res.status(401).json({
+            status: "error",
+            message: "Usuario no autenticado"
+        });
         }
-            next()
-    }
-}
+
+        if (!allowedRoles.includes(req.user.role)) {
+        return res.status(403).json({
+            status: "error",
+            message: "No tenés permisos para realizar esta acción"
+        });
+        }
+
+        next();
+    };
+};
