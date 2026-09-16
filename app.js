8715187814a74a861d5aa35eab2334123b7c2dfc
enzo herrera <enzoarielherreraortega97@gmail.com>
2026-09-15 23:32:45 -0300

    Recuperacion de app.js README y .env.example de la pre-entrega 5

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ import eventsRouter from "./src/routes/events.router.js";
 import sessionsRouter from "./src/routes/sessions.router.js";
 import cookieParser from "cookie-parser";
 import passport from "./src/config/passport.config.js";
+import { errorHandler } from "./src/middlewares/errorHandler.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(passport.initialize());
+app.use(errorHandler);
 app.use(express.urlencoded({ extended: true }));
-
 
 app.get("/api/health", (req, res) => {
     res.json({
@@ -22,7 +23,5 @@ app.get("/api/health", (req, res) => {
 
 app.use("/api/events", eventsRouter);
 app.use("/api/sessions", sessionsRouter);
-
-
 
 export default app;
